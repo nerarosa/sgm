85f83c728718fdffae40f1b1703ee7d7d4968b09
nerarosa <tienhv0502@gmail.com>
2017-07-15 20:44:26 +0700

    add comic js

---
+++ mini/js/item-comic.js
@@ -0,0 +1,128 @@
+/* jQuery Shorten plugin 1.0.0 */
+(function($) {
+	$.fn.shorten = function (settings) {
+	
+		var config = {
+			showChars: 100,
+			ellipsesText: "...",
+			moreText: "more",
+			lessText: "less"
+		};
+
+		if (settings) {
+			$.extend(config, settings);
+		}
+		
+		$(document).off("click", '.morelink');
+		
+		$(document).on({click: function () {
+
+				var $this = $(this);
+				if ($this.hasClass('less')) {
+					$this.removeClass('less');
+					$this.html(config.moreText);
+				} else {
+					$this.addClass('less');
+					$this.html(config.lessText);
+				}
+				$this.parent().prev().toggle();
+				$this.prev().slideToggle();
+				return false;
+			}
+		}, '.morelink');
+
+		return this.each(function () {
+			var $this = $(this);
+			if($this.hasClass("shortened")) return;
+			
+			$this.addClass("shortened");
+			var content = $this.html();
+			if (content.length > config.showChars) {
+				var c = content.substr(0, config.showChars);
+				var h = content.substr(config.showChars, content.length - config.showChars);
+				var html = c + '<span class="moreellipses"> </span><span class="morecontent"><span>' + h + '</span> <a href="#" class="morelink">' + config.ellipsesText + '' + config.moreText + '</a></span>';
+				$this.html(html);
+				$(".morecontent span").hide();
+			}
+		});
+		
+	};
+
+ })(jQuery);
+
+$(document).ready(function(){
+	var mainData = $(".comic-post-content").text(),
+		auth = sgmTags("author", mainData),
+		transtor = sgmTags("trans", mainData),
+		des = sgmTags("des", mainData),
+		chap = sgmTags("chap", mainData);
+	
+	$(".comic-author .value").text(auth);
+	$(".comic-translator .value").text(transtor);
+	$(".comic-des .comic-sum").text(des);
+	
+	$(".comic-sum").shorten({
+		"showChars" : 200,
+		"moreText"  : "Xem thêm",
+		"lessText"  : "Rút gọn"
+	});
+	
+	function getChapter(url){
+		$.ajax({
+			url: url,
+			type: "GET",
+			dataType: "jsonp",
+			beforeSend: function(){
+				$(".comic-chap-list").html("Data is Loading...");
+			},
+			success: function(data){
+				var htmlChap = '', nextUrl = '',
+					entry = data.feed.entry,
+					links = data.feed.link;
+					
+				for(var i=0; i< links.length; i++){
+					if(links[i].rel == "next") nextUrl = links[i].href.split("?")[1];
+				}
+				
+				if(entry !== undefined){
+					var idChap = '', dateChap = '', titleChap = '';
+					for(var i = 0; i < entry.length; i++){
+						titleChap = entry[i].title.$t;
+						idChap = entry[i].id.$t.split('post-')[1];
+						dateChap = entry[i].published.$t;
+					
+						var date = new Date(dateChap);
+						dateChap = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
+						
+						htmlChap += '<div class="row"><span><a href="' + url_blog + '/p/reader.html?id='+idMain + '&idc=' + idChap + '&sv=' + url.split('://')[1].split('.')[0] +'" target="_blank" title="'+ titleChap +'">'+ titleChap +'</a></span><span>'+ dateChap +'</span></div>';
+						
+						if(i==0)
+							$('.start.btn').attr('href', '/p/reader.html?id='+idMain + '&idc=' + idChap + '&sv=' + url.split('://')[1].split('.')[0]);
+					}
+					
+					if(nextUrl != ''){
+						var next = url.split("?")[0] + "?" + nextUrl;
+						getChapter(next);
+					}
+				}else{
+					htmlChap = "Đang cập nhật";
+				}
+				if($(".comic-chap-list row").length > 0)
+					$(".comic-chap-list").append(htmlChap);
+				else
+					$(".comic-chap-list").html(htmlChap);
+			},
+			error: function(){
+				$(".comic-chap-list").html("Error load data");
+			}
+		});
+	}
+	
+	if(chap != ''){
+		var urlData = "http://" + chap.split(";")[0] + ".blogspot.com",
+			nameComic = chap.split(";")[1];
+		var urlSv = urlData + "/feeds/posts/default/-/" + nameComic + "?alt=json-in-script&max-results=100&orderby=published";
+		getChapter(urlSv);
+	}
+	
+});
